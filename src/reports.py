9379246ecb93a02362f514f9acc91503795c9eb5
Bugrova Irina <bugrova147@yandex.ru>
2025-01-07 23:34:06 +0300

    add main.py, fixed other funcs

--- src/reports.py
+++ src/reports.py
@@ -1,9 +1,6 @@
-import json
 import logging
 import os
-
 from datetime import datetime, timedelta
-from functools import wraps
 from typing import Optional
 
 import pandas as pd
@@ -22,7 +19,7 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def get_transactions_xlsx():
+def get_transactions_from_xlsx():
     """Считывание финансовых операций из xlsx-файла"""
     try:
         transactions = pd.read_excel("../coursework_1/data/operations.xlsx")
@@ -32,7 +29,9 @@ def get_transactions_xlsx():
         return f"Произошла ошибка {ex}"
 
 
-def get_spending_by_categories(transactions: pd.DataFrame, category: str, end_date: Optional[str] = None) -> pd.DataFrame:
+def get_spending_by_categories(
+    transactions: pd.DataFrame, category: str, end_date: Optional[str] = None
+) -> pd.DataFrame:
     """
     Возвращает траты по заданной категории
     за последние три месяца (от переданной даты)
@@ -53,21 +52,21 @@ def get_spending_by_categories(transactions: pd.DataFrame, category: str, end_da
 
         # фильтруем транзакции
         filtered_transactions = transactions[
-            (transactions["Статус"] == "OK") &
-            (transactions["Сумма операции"] < 0) &
-            (transactions["Категория"] == category) &
-            (transactions["Дата операции"] >= start_date) &
-            (transactions["Дата операции"] <= end_date)
+            (transactions["Статус"] == "OK")
+            & (transactions["Сумма операции"] < 0)
+            & (transactions["Категория"] == category)
+            & (transactions["Дата операции"] >= start_date)
+            & (transactions["Дата операции"] <= end_date)
         ]
 
         # проверка на пустой датафрейм
         if filtered_transactions.empty:
             return pd.DataFrame(columns=transactions.columns)
 
-        # преобразуем дату операции в строку для сериализации
-        # filtered_transactions["Дата операции"] = filtered_transactions["Дата операции"].df.strftime('%d.%m.%Y %H:%M:%S')
+        # json_result = filtered_transactions.to_json(orient="records", indent=4, force_ascii=False)
 
         return filtered_transactions
+        # return json_result
 
     except Exception as ex:
         logger.error(f"Произошла ошибка: {ex}")
@@ -76,14 +75,15 @@ def get_spending_by_categories(transactions: pd.DataFrame, category: str, end_da
 
 def get_json_file(func):
     """
-    Записывает полученный ранее отчет в формате датафрейма в json-файл
+    Записывает полученный ранее отчет в формате датафрейма
+    в json-файл report.json
     """
+
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
 
         if isinstance(result, pd.DataFrame):
-            wrapper_result = result.to_dict("records") # преобразуем датафрейм в список словарей
-            wrapper_result = json.dumps(wrapper_result, indent=4, ensure_ascii=False)
+            wrapper_result = result.to_json(orient="records", indent=4, force_ascii=False)
             os.makedirs(os.path.dirname("../coursework_1/reports/report.json"), exist_ok=True)
             with open("../coursework_1/reports/report.json", "w", encoding="utf-8") as file:
                 file.write(wrapper_result)
@@ -96,6 +96,11 @@ def get_json_file(func):
 
 
 def get_name_json_file(file_name):
+    """
+    Записывает полученный ранее отчет в формате датафрейма
+    в json-файл, имя которого задает пользователь
+    """
+
     def my_decorator(func):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
@@ -103,30 +108,38 @@ def get_name_json_file(file_name):
             if isinstance(result, pd.DataFrame):
                 os.makedirs(os.path.dirname("../coursework_1/reports"), exist_ok=True)
                 with open(os.path.join("../coursework_1/reports", file_name), "w", encoding="utf-8") as file:
-                    json.dump(result.to_dict("records"), file, ensure_ascii=False, indent=4)
-                return result
+                    json_result = result.to_json(file, orient="records", indent=4, force_ascii=False)
+
+                return json_result
             else:
                 logger.error(f"Функция {func.__name__} вернула ошибку: {result}")
                 raise ValueError(f"Функция {func.__name__} вернула ошибку: {result}")
 
         return wrapper
+
     return my_decorator
 
 
 @get_json_file
 def generate_report(transactions: pd.DataFrame, category: str, end_date: Optional[str] = None) -> pd.DataFrame:
+    """
+    выдает отчет в файл report.json
+    """
     result_df = get_spending_by_categories(transactions, category, end_date)
 
-    result_df["Дата операции"] = result_df["Дата операции"].dt.strftime('%d.%m.%Y %H:%M:%S')
+    result_df.loc[:, "Дата операции"] = result_df["Дата операции"].dt.strftime("%d.%m.%Y %H:%M:%S")
 
     return result_df
 
 
 @get_name_json_file("my_report.json")
 def generate_my_report(transactions: pd.DataFrame, category: str, end_date: Optional[str] = None) -> pd.DataFrame:
+    """
+    выдает отчет в файл my_report.json
+    """
     result_df = get_spending_by_categories(transactions, category, end_date)
 
-    result_df["Дата операции"] = result_df["Дата операции"].dt.strftime('%d.%m.%Y %H:%M:%S')
+    result_df.loc[:, "Дата операции"] = result_df["Дата операции"].dt.strftime("%d.%m.%Y %H:%M:%S")
 
     return result_df
 
@@ -134,12 +147,10 @@ def generate_my_report(transactions: pd.DataFrame, category: str, end_date: Opti
 if __name__ == "__main__":
     # print(get_transactions_xlsx())
     # print(get_spending_by_categories(get_transactions_xlsx(), "Супермаркеты", "30.12.2021 19:06:39"))
-    # print(type(get_spending_by_categories(get_transactions_xlsx(), "Супермаркеты", "30.12.2021 19:06:39")))
-    transactions_df = get_transactions_xlsx()
-    # print(transactions_df)
+    transactions_df = get_transactions_from_xlsx()
 
     report = generate_report(transactions_df, "Супермаркеты", "30.12.2021 19:06:39")
-    print(f"Стандартный отчет записан в report.json")
+    print("Стандартный отчет записан в report.json")
 
     my_report = generate_my_report(transactions_df, "Супермаркеты", "30.12.2021 19:06:39")
-    print(f"Отчет записан в выбранный пользователем файл my_report.json")
+    print("Отчет записан в выбранный пользователем файл my_report.json")
